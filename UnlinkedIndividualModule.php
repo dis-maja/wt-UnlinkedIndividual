5e41df2c56353fbc1675495dfd477399085129da
dis-maja <80508009+dis-maja@users.noreply.github.com>
2021-10-24 12:47:31 +0200

    Update UnlinkedIndividualModule.php
    new version

--- UnlinkedIndividualModule.php
+++ UnlinkedIndividualModule.php
@@ -138,7 +138,7 @@ class UnlinkedIndividualModule extends AbstractModule implements ModuleCustomInt
      */
     public function customModuleVersion(): string
     {
-	return '2.0.3';
+	return '2.0.4';
     }
 
     /**
